2e71e5a7d383e3e78a45ca37de5f140803c40b59
Pedro Igor <pedroigor.silva@outlook.com>
2025-05-13 12:55:07 -0300

    feat: join call with muted devices

--- src/Controls/Local/LocalAudioMute.tsx
+++ src/Controls/Local/LocalAudioMute.tsx
@@ -6,12 +6,28 @@ import PropsContext, { ToggleState } from '../../PropsContext'
 import muteAudio from './muteAudio'
 
 function LocalAudioMute() {
-  const { styleProps, callbacks } = useContext(PropsContext)
+  const {
+    styleProps,
+    callbacks,
+    rtcProps: { enableAudio }
+  } = useContext(PropsContext)
   const { localBtnStyles } = styleProps || {}
   const { muteLocalAudio } = localBtnStyles || {}
   const { dispatch, localAudioTrack } = useContext(RtcContext)
   const local = useContext(LocalContext)
-
+  const [isFirstRender, setIsFirstRender] = React.useState(!enableAudio)
+  React.useEffect(() => {
+    const mute = async () => {
+      if (isFirstRender && localAudioTrack) {
+        await muteAudio(local, dispatch, localAudioTrack, callbacks).then(
+          () => {
+            setIsFirstRender(false)
+          }
+        )
+      }
+    }
+    mute()
+  }, [isFirstRender, localAudioTrack, muteAudio, local, dispatch, callbacks])
   return (
     <div>
       <BtnTemplate
